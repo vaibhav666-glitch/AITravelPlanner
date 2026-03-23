6bce3d78fae5ff89981b3c3c23cdf31b044081e9
vaibhav <vaibhavning@gmail.com>
2026-03-24 00:09:47 +0530

    update home page

--- Frontend/my-app/src/app/page.tsx
+++ Frontend/my-app/src/app/page.tsx
@@ -1,5 +1,20 @@
-import { redirect } from "next/navigation";
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  redirect("/dashboard");
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    if (!token) {
+      router.replace("/login");
+    } else {
+      router.replace("/dashboard");
+    }
+  }, [router]);
+
+  return null; 
 }
